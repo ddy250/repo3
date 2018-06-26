bc4f1c00a52b951e06a9f372143d46168f9ad692
ddy250 <odedsagi@gmail.com>
2018-06-26 11:29:14 +0300

    version3

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var resourcesRouter= require('./routes/resources');
-var dataRouter= require('./routes/data');
-var platformRouter= require('./routes/platform');
-var pricingRouter= require('./routes/pricing');
+
 
 var app = express();
 
@@ -28,9 +26,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/resources', resourcesRouter);
-app.use('/data', dataRouter);
-app.use('/platform', platformRouter);
-app.use('/pricing', pricingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
